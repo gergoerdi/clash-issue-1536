a92368cac0681d05adf741ced77e5323b6021c6f
Dr. ERDI Gergo <gergo@erdi.hu>
2020-12-09 18:48:19 +0800

    Simplify a bit more

--- src/Board.hs
+++ src/Board.hs
@@ -31,7 +31,7 @@ instance Semigroup (AddrMap s dom) where
 
 newtype Addressing s dom dat addr a = Addressing
     { unAddressing :: RWS
-          (FanIn dom addr, Signal dom (Maybe dat), AddrMap s dom)
+          (FanIn dom addr, AddrMap s dom)
           (FanIn dom (Maybe dat), AddrMap s dom)
           Int
           a
@@ -45,70 +45,32 @@ memoryMap
     -> (Signal dom (Maybe dat), a)
 memoryMap addr wr body = (join <$> firstIn read, x)
   where
-    (x, (read, conns)) = evalRWS (unAddressing body) (fanInMaybe addr, wr, conns) 0
-
-readWrite
-    :: (HiddenClockResetEnable dom, Typeable addr')
-    => (Signal dom (Maybe addr') -> Signal dom (Maybe dat) -> (Signal dom (Maybe dat), a))
-    -> Addressing s dom dat addr (Component s addr', a)
-readWrite mkComponent = Addressing $ do
-    component <- Component typeRep <$> get <* modify succ
-    (_, wr, addrs) <- ask
-    let addr = firstIn . fromMaybe mempty $ DMap.lookup component (addrMap addrs)
-        selected = isJust <$> addr
-    let (read, x) = mkComponent addr wr
-    tell (gated (delay False selected) (fanIn read), mempty)
-    return (component, x)
+    (x, (read, conns)) = evalRWS (unAddressing body) (fanInMaybe addr, conns) 0
 
 readWrite_
     :: (HiddenClockResetEnable dom, Typeable addr')
-    => (Signal dom (Maybe addr') -> Signal dom (Maybe dat) -> Signal dom (Maybe dat))
+    => (Signal dom (Maybe addr') -> Signal dom (Maybe dat))
     -> Addressing s dom dat addr (Component s addr')
-readWrite_ mkComponent = fmap fst $ readWrite $ \addr wr -> (mkComponent addr wr, ())
+readWrite_ mkComponent = Addressing $ do
+    component <- Component typeRep <$> get <* modify succ
+    (_, addrs) <- ask
+    let addr = firstIn . fromMaybe mempty $ DMap.lookup component (addrMap addrs)
+        read = mkComponent addr
+    tell (fanIn read, mempty)
+    return component
 
 ram0
     :: (HiddenClockResetEnable dom, 1 <= n, NFDataX dat, Num dat)
     => SNat n
     -> Addressing s dom dat addr (Component s (Index n))
-ram0 size@SNat = readWrite_ $ \addr wr ->
-      fmap Just $ blockRam1 ClearOnReset size 0 (fromMaybe 0 <$> addr) (liftA2 (,) <$> addr <*> wr)
-
-matchAddr
-    :: (addr -> Maybe addr')
-    -> Addressing s dom dat addr' a
-    -> Addressing s dom dat addr a
-matchAddr match body = Addressing $ rws $ \(addr, wr, addrs) s ->
-  let addr' = fanInMaybe . fmap (match =<<) . firstIn $ addr
-      (x, s', (read, components)) = runRWS (unAddressing body) (addr', wr, addrs) s
-  in (x, s', (read, components))
-
-gated :: Signal dom Bool -> FanIn dom a -> FanIn dom a
-gated p sig = fanInMaybe $ mux p (firstIn sig) (pure Nothing)
-
-tag
-    :: addr'
-    -> Addressing s dom dat (addr', addr) a
-    -> Addressing s dom dat addr a
-tag t = matchAddr $ \addr -> Just (t, addr)
-
-from
-    :: forall addr' s dom dat addr a. (Integral addr, Ord addr, Integral addr', Bounded addr')
-    => addr
-    -> Addressing s dom dat addr' a
-    -> Addressing s dom dat addr a
-from base = matchAddr $ \addr -> do
-    guard $ addr >= base
-    let offset = addr - base
-    guard $ offset <= lim
-    return $ fromIntegral offset
-  where
-    lim = fromIntegral (maxBound :: addr')
+ram0 size@SNat = readWrite_ $ \addr ->
+    fmap Just $ blockRam1 ClearOnReset size 0 (fromMaybe 0 <$> addr) (pure Nothing)
 
 connect
     :: Component s addr
     -> Addressing s dom dat addr ()
 connect component@(Component _ i) = Addressing $ do
-    (addr, _, _) <- ask
+    (addr, _) <- ask
     tell (mempty, AddrMap $ DMap.singleton component addr)
 
 firstIn :: FanIn dom a -> Signal dom (Maybe a)
@@ -123,11 +85,9 @@ fanIn = fanInMaybe . fmap pure
 topEntity
     :: Clock System
     -> Reset System
-    -> Signal System (Maybe (Unsigned 16))
+    -> Signal System (Maybe (Index 0x0400))
     -> Signal System (Maybe (Unsigned 8))
     -> (Signal System (Maybe (Unsigned 8)), ())
 topEntity clk rst addr wr = withClockResetEnable clk rst enableGen $ memoryMap addr wr $ do
     ram <- ram0 (SNat @0x0400)
-
-    from 0x2000 $ connect ram
-    from 0x4000 $ connect ram
+    connect ram
