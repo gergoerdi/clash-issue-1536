e88794d0dba353a5add57b5faf9bac0ac3d36f08
Dr. ERDI Gergo <gergo@erdi.hu>
2020-12-22 20:48:12 +0800

    Clean up `build.sh`

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 stack build
 stack exec -- clash -isrc -outputdir _build/ --verilog src/Board.hs \
-      -fclash-debug DebugSilent \
       -fclash-inline-limit=200 \
-      -fclash-spec-limit=100 \
+      -fclash-spec-limit=30 \
       2>&1 | tee log.txt
