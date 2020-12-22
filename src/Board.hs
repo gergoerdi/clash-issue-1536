6a042c5780b603d0d250084e3af3b687ed02a2b8
Dr. ERDI Gergo <gergo@erdi.hu>
2020-12-22 20:48:39 +0800

    working base version

--- src/Board.hs
+++ src/Board.hs
@@ -1,93 +1,88 @@
 {-# LANGUAGE DerivingStrategies, GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE RankNTypes #-}
 module Board where
 
 import Clash.Prelude
 import Data.Maybe
 import Control.Monad
 import Control.Monad.RWS
-
-import Unsafe.Coerce
 import Assoc as Map
 
-type Key = Unsigned 32
-newtype Component s addr = Component Key
+newtype Component addr = Component Int
     deriving newtype (Eq, Ord)
 
-newtype FanIn dom a = FanIn{ getFanIn :: Signal dom `Ap` First a }
+newtype FanIn a = FanIn{ getFanIn :: First a }
     deriving newtype (Semigroup, Monoid)
 
-newtype AddrMap s dom = AddrMap{ addrMap :: Map Key (FanIn dom ()) }
+newtype AddrMap = AddrMap{ addrMap :: [(Int,(FanIn (Index 0x0400)))] }
     deriving newtype (Monoid)
 
-instance Semigroup (AddrMap s dom) where
-    AddrMap map1 <> AddrMap map2 = AddrMap $ unionWithKey (const mappend) map1 map2
+instance Semigroup (AddrMap) where
+    AddrMap map1 <> AddrMap map2 = AddrMap $ Map.unionWithKey (const mappend) map1 map2
 
-newtype Addressing s dom dat addr a = Addressing
+newtype Addressing dat addr a = Addressing
     { unAddressing :: RWS
-          (FanIn dom addr, AddrMap s dom)
-          (FanIn dom (Maybe dat), AddrMap s dom)
-          Key
+          (FanIn addr, AddrMap)
+          (FanIn (Maybe dat), AddrMap)
+          Int
           a
     }
     deriving newtype (Functor, Applicative, Monad)
 
 memoryMap
-    :: Signal dom (Maybe addr)
-    -> (forall s. Addressing s dom dat addr a)
-    -> (Signal dom (Maybe dat), a)
-memoryMap addr body = (join <$> firstIn read, x)
+    :: Maybe addr
+    -> (Addressing dat addr a)
+    -> (Maybe dat, a)
+memoryMap addr body = (join (firstIn read), x)
   where
     (x, (read, conns)) = evalRWS (unAddressing body) (fanInMaybe addr, conns) 0
 
 readWrite_
-    :: (HiddenClockResetEnable dom)
-    => (Signal dom (Maybe addr') -> Signal dom (Maybe dat))
-    -> Addressing s dom dat addr (Component s addr')
+    :: ((Maybe (Index 1024)) -> (Maybe dat))
+    -> Addressing dat addr (Component (Index 1024))
 readWrite_ mkComponent = Addressing $ do
     component@(Component i) <- Component <$> get <* modify succ
     (_, addrs) <- ask
     let addr = firstIn . fromMaybe mempty $ Map.lookup i (addrMap addrs)
-        read = mkComponent $ unsafeCoerce addr
+        read = mkComponent addr
     tell (fanIn read, mempty)
     return component
 
 ram0
-    :: (HiddenClockResetEnable dom, 1 <= n, NFDataX dat, Num dat)
+    :: (1 <= n)
     => SNat n
-    -> Addressing s dom dat addr (Component s (Index n))
+    -> Addressing (Index 1024) addr (Component (Index 1024))
 ram0 size@SNat = readWrite_ $ \addr ->
-    fmap Just $ blockRam1 ClearOnReset size 0 (fromMaybe 0 <$> addr) (pure Nothing)
+    Just $ negate (fromMaybe 2 addr)
 
 connect
-    :: Component s addr
-    -> Addressing s dom dat addr ()
+    :: Component (Index 1024)
+    -> Addressing dat (Index 1024) ()
 connect component@(Component i) = Addressing $ do
     (addr, _) <- ask
-    tell (mempty, AddrMap $ Map.singleton i $ unsafeCoerce addr)
+    tell (mempty, AddrMap $ Map.singleton i $ addr)
 
-firstIn :: FanIn dom a -> Signal dom (Maybe a)
-firstIn = fmap getFirst . getAp . getFanIn
+firstIn :: FanIn a -> Maybe a
+firstIn = getFirst . getFanIn
 
-fanInMaybe :: Signal dom (Maybe a) -> FanIn dom a
-fanInMaybe = FanIn . Ap . fmap First
+fanInMaybe :: Maybe a -> FanIn a
+fanInMaybe = FanIn . First
 
-fanIn :: Signal dom a -> FanIn dom a
-fanIn = fanInMaybe . fmap pure
+fanIn :: a -> FanIn a
+fanIn = fanInMaybe . pure
 
 matchAddr
     :: (addr -> Maybe addr')
-    -> Addressing s dom dat addr' a
-    -> Addressing s dom dat addr a
+    -> Addressing dat addr' a
+    -> Addressing dat addr a
 matchAddr match body = Addressing $ rws $ \(addr, addrs) s ->
-  let addr' = fanInMaybe . fmap (match =<<) . firstIn $ addr
+  let addr' = fanInMaybe . (match =<<) . firstIn $ addr
   in runRWS (unAddressing body) (addr', addrs) s
 
 from
-    :: forall addr' s dom dat addr a. (Integral addr, Ord addr, Integral addr', Bounded addr')
+    :: forall addr' dat addr a. (Integral addr, Ord addr, Integral addr', Bounded addr')
     => addr
-    -> Addressing s dom dat addr' a
-    -> Addressing s dom dat addr a
+    -> Addressing dat addr' a
+    -> Addressing dat addr a
 from base = matchAddr $ \addr -> do
     guard $ addr >= base
     let offset = addr - base
@@ -97,11 +92,9 @@ from base = matchAddr $ \addr -> do
     lim = fromIntegral (maxBound :: addr')
 
 topEntity
-    :: Clock System
-    -> Reset System
-    -> Signal System (Maybe (Index 0x0800))
-    -> (Signal System (Maybe (Unsigned 8)), ())
-topEntity clk rst addr = withClockResetEnable clk rst enableGen $ memoryMap addr $ do
+    :: Maybe (Index 0x0400)
+    -> ((Maybe (Index 0x0400)), ())
+topEntity addr = memoryMap addr $ do
     ram <- ram0 (SNat @0x0400)
     from 0x0000 $ connect ram
     from 0x0400 $ connect ram
