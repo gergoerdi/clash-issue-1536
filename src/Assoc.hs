d1a53e321c31da3cace9efd52c8d81a5dbdafe0f
Dr. ERDI Gergo <gergo@erdi.hu>
2020-12-23 12:58:43 +0800

    Add `Monoid` instances to assoc-list-backed maps

--- src/Assoc.hs
+++ src/Assoc.hs
@@ -2,10 +2,19 @@ module Assoc where
 
 import Clash.Prelude hiding (lookup, fold)
 
-type Map k a = [(k, a)]
+newtype Map k a = MkMap{ assocs :: [(k, a)] }
 
-unionWithKey :: Ord k => (k -> a -> a -> a) -> Map k a -> Map k a -> Map k a
-unionWithKey f l r = fold (insertWithKey f) r l
+instance (Ord k) => Semigroup (Map k a) where
+    (<>) = union
+
+instance (Ord k) => Monoid (Map k a) where
+    mempty = MkMap []
+
+unionWithKey :: (Ord k) => (k -> a -> a -> a) -> Map k a -> Map k a -> Map k a
+unionWithKey f l r = fold (insertWithKey f) r (assocs l)
+
+union :: (Ord k) => Map k a -> Map k a -> Map k a
+union = unionWithKey (\ _ old new -> old)
 
 fold :: (a -> b -> b) -> b -> [a] -> b
 fold f = go
@@ -13,8 +22,8 @@ fold f = go
     go z [] = z
     go z (x:xs) = go (f x z) xs
 
-insertWithKey :: Ord k => (k -> a -> a -> a) -> (k,a) -> Map k a -> Map k a
-insertWithKey f x0@(k0, v0) = go
+insertWithKey :: (Ord k) => (k -> a -> a -> a) -> (k,a) -> Map k a -> Map k a
+insertWithKey f x0@(k0, v0) = MkMap . go . assocs
   where
     go [] = [x0]
     go (x1@(k1, v1) : xs) = case compare k0 k1 of
@@ -22,11 +31,11 @@ insertWithKey f x0@(k0, v0) = go
         EQ -> (k0, f k0 v0 v1) : xs
         GT -> x1 : go xs
 
-lookup :: Eq k => k -> Map k a -> Maybe a
-lookup k0 = go
+lookup :: (Eq k) => k -> Map k a -> Maybe a
+lookup k0 = go . assocs
   where
     go [] = Nothing
     go ((k1,v1):xs) = if k1 == k0 then Just v1 else go xs
 
 singleton :: k -> a -> Map k a
-singleton k a = [(k, a)]
+singleton k a = MkMap [(k, a)]
--- src/DAssoc.hs
+++ src/DAssoc.hs
@@ -8,19 +8,25 @@ import Type.Reflection
 
 import Clash.Prelude hiding (lookup, fold)
 
-type DMap k f = [DSum k f]
+newtype DMap k f = MkDMap { dassocs :: [DSum k f] }
+
+instance (GCompare k) => Semigroup (DMap k f) where
+    (<>) = union
+
+instance (GCompare k) => Monoid (DMap k f) where
+    mempty = MkDMap []
 
 unionWithKey :: (GCompare k) => (forall a. k a -> f a -> f a -> f a) -> DMap k f -> DMap k f -> DMap k f
-unionWithKey f l r = case l of
-    [] -> r
-    ((k :=> v) : xs) -> unionWithKey f (insertWithKey f k v l) xs
+unionWithKey f l r = go r (dassocs l)
+  where
+    go r [] = r
+    go r ((k :=> v) : xs) = go (insertWithKey f k v l) xs
 
-fold :: (a -> b -> b) -> b -> [a] -> b
-fold _ z []     = z
-fold f z (x:xs) = fold f (f x z) xs
+union :: (GCompare k) => DMap k f -> DMap k f -> DMap k f
+union = unionWithKey $ \ _ old new -> old
 
 insertWithKey :: (GCompare k) => (k a -> f a -> f a -> f a) -> k a -> f a -> DMap k f -> DMap k f
-insertWithKey f k0 v0 = go
+insertWithKey f k0 v0 = MkDMap . go . dassocs
   where
     go [] = [k0 :=> v0]
     go ((k1 :=> v1) : xs) = case gcompare k0 k1 of
@@ -29,10 +35,12 @@ insertWithKey f k0 v0 = go
         GGT -> (k1 :=> v1) : go xs
 
 lookup :: (GCompare k) => k a -> DMap k f -> Maybe (f a)
-lookup k0 [] = Nothing
-lookup k0 ((k :=> v) : xs) = case geq k0 k of
-    Just Refl -> Just v
-    Nothing -> lookup k0 xs
+lookup k0 = go . dassocs
+  where
+    go [] = Nothing
+    go ((k :=> v) : xs) = case geq k0 k of
+        Just Refl -> Just v
+        Nothing -> go xs
 
 singleton :: k a -> f a -> DMap k f
-singleton k v = [k :=> v]
+singleton k v = MkDMap [k :=> v]
