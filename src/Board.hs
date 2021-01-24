b2f1fcc58d3f83f8b274d2abe77754b4a5d74d8f
Dr. ERDI Gergo <gergo@erdi.hu>
2021-01-24 11:14:51 +0800

    a single return

--- src/Board.hs
+++ src/Board.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 module Board where
 
 import Clash.Prelude
@@ -12,51 +11,20 @@ peripherals
     -> Signal dom (Maybe (Unsigned 8))
 peripherals dips cmd = pure Nothing
 
-#define ROM 1
-#define RAM 1
-#define VID 0
-#define PORT 0
-
 topEntity
     :: (HiddenClockResetEnable System)
     => Signal System (BitVector 8)
     -> Signal System (Maybe (Unsigned 8))
     -> Signal System (Maybe (Either (Unsigned 8) (Unsigned 16)))
     -> Signal System (Maybe (Unsigned 8))
-    -> (Signal System (Maybe (Unsigned 8)), (Signal System (Maybe (Index 7168)), Signal System (Maybe (Unsigned 8))))
-topEntity dips vidRead addr wr = memoryMap addr wr $ do
-#if ROM
+    -> Signal System (Maybe (Unsigned 8))
+topEntity dips vidRead addr wr = memoryMap_ addr wr $ do
     rom <- romFromFile (SNat @0x2000) "_build/SpaceInvaders.bin"
-#endif
-#if RAM
     ram <- ram0 (SNat @0x0400)
-#endif
-#if VID
-    (vid, vidAddr, vidWrite) <- conduit vidRead
-#else
-    let vidAddr = pure Nothing
-        vidWrite = pure Nothing
-#endif
-
-#if PORT
-    io <- port_ $ peripherals dips
-
-    matchLeft $ do
-        from 0x00 $ connect io
-#endif
 
     matchRight $ do
-#if ROM
         from 0x0000 $ connect rom
-#endif
-#if RAM
         from 0x2000 $ connect ram
-#endif
-#if VID
-        from 0x2400 $ connect vid
-#endif
-#if RAM
         from 0x4000 $ connect ram
-#endif
 
-    return (vidAddr, vidWrite)
+    -- return ()
